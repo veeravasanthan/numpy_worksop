cac8da37189d6fc516feb6bb16e4424353927149
veeravasanthan <153995892+veeravasanthan@users.noreply.github.com>
2024-05-22 10:45:17 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,9 @@
 #write a program to find the factorial of a nummber
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_recursive(n - 1)
+        
+number = 5
+print(f'The factorial of {number} is {factorial_recursive(number)}.')
