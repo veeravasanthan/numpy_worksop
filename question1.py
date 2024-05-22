ba712cbf480e27d2fd86118722c0a3e72725e428
veeravasanthan <153995892+veeravasanthan@users.noreply.github.com>
2024-05-22 10:56:43 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,6 +1,6 @@
 # write a python code to find if the given number is odd or even?
-num=int(input(enter a number))
-if num%2==0
+num=int(input("enter a number"))
+if num%2==0:
    print("it is an even")
-else
+else:
     print("it is an odd")
