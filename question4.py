b73ed2f73ff45ee2c100065de318c79cd9fd188b
veeravasanthan <153995892+veeravasanthan@users.noreply.github.com>
2024-05-22 10:45:51 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,10 @@
 #write a program to find the sum of digits of a given number'
+def getSum(n): 
+	
+	sum = 0
+	for digit in str(n): 
+	sum += int(digit)	 
+	return sum
+
+n = 12345
+print(getSum(n))
